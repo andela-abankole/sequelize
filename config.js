274c1deb206e5c8bc0b00e9037fd87f3a7e7fd68
andela-abankole <akinjide.bankole@andela.co>
2015-10-30 20:45:28 +0100

    feat (config): contain database helpers

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'database' : process.env.MY_SQL_DEV_DATABASE,
+  'host' : process.env.MY_SQL_DEV_HOST,
+  'username' : process.env.MY_SQL_DEV_USERNAME,
+  'port' : process.env.MY_SQL_DEV_PORT,
+  'password' : process.env.MY_SQL_DEV_PASSWORD
+};
